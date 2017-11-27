2adbb65eda50f89860794a007a914f1edffc4767
Michael Park <michael.j.park@gatech.edu>
2017-11-27 14:05:42 -0500

    took apart mfcc reconstruction, now i know how to change the parameters for different audio frame sizes

--- MFCC_TEST/mfcc_test.py
+++ MFCC_TEST/mfcc_test.py
@@ -26,17 +26,14 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
+N_MFCC = 3
+
 def invlogamplitude(S):
 #"""librosa.logamplitude is actually 10_log10, so invert that."""
     return 10.0**(S/10.0)
 
 def audio_creator(): 
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    RECORD_SECONDS = 2
-    WAVE_OUTPUT_FILENAME = "data20crop4.wav" #n_mfcc=24
+    WAVE_OUTPUT_FILENAME = "data['1'].wav" #n_mfcc=24
     #WAVE_OUTPUT_FILENAME = "AudioBookCrop.wav" #n_mfcc=12
     #WAVE_OUTPUT_FILENAME = "data5crop4.wav"  #n_mfcc=2
     
@@ -49,25 +46,35 @@ def audio_creator():
 
     #####################################
     # Original #
-    mfccs = librosa.feature.mfcc(y, n_mfcc=2)# default hop_length=512, hop_length=int(0.01*sr))
-    print mfccs
+    # if unspecified win_length=n_fft
+    # hop_length=win_length/4
+    # n_fft = fft window size
+    # 
+    mfccs = librosa.feature.mfcc(y=y, sr=44100, hop_length=1024, n_fft=4096, n_mfcc=N_MFCC)# default hop_length=512, hop_length=int(0.01*sr))
+    print 'mfccs shape'
     print mfccs.shape
     ############################
 
     #build reconstruction mappings
     n_mfcc = mfccs.shape[0]
-    n_mel = 128
+    n_mel = 128 #just using n_mel = hop_length/4 based on the default values given in code
     dctm = librosa.filters.dct(n_mfcc, n_mel)
-    n_fft = 2048
-    mel_basis = librosa.filters.mel(sr, n_fft)
+    n_fft = 4096
+    mel_basis = librosa.filters.mel(sr, n_fft, n_mels=n_mel)
 
     #Empirical scaling of channels to get ~flat amplitude mapping.
     bin_scaling = 1.0/np.maximum(0.0005, np.sum(np.dot(mel_basis.T, mel_basis), axis=0))
     #Reconstruct the approximate STFT squared-magnitude from the MFCCs.
+    print bin_scaling[:, np.newaxis].shape
+    print '------'
+    print mel_basis.T.shape
+    print np.dot(dctm.T, mfccs).shape
+    print np.dot(mel_basis.T, invlogamplitude(np.dot(dctm.T, mfccs))).shape
     recon_stft = bin_scaling[:, np.newaxis] * np.dot(mel_basis.T, invlogamplitude(np.dot(dctm.T, mfccs)))
     #Impose reconstructed magnitude on white noise STFT.
     excitation = np.random.randn(y.shape[0])
-    E = librosa.stft(excitation)
+    print y.shape[0]
+    E = librosa.stft(excitation, n_fft=n_fft)
     recon = librosa.istft(E/np.abs(E)*np.sqrt(recon_stft))
     #print recon
     #print recon.shape
